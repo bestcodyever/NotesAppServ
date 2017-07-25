72cced815f013483a7987ee7dd0eb662f97c5950
Cody Duskin <codyduskin@me.com>
2017-07-25 11:43:39 -0600

    start

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,5 @@
-const environment = process.env.NODE_ENV || 'production'
+const environment = process.env.NODE_ENV || 'development';
 
-const config = require('../knexfile')[environment]
+const config = require('../knexfile')[environment];
 
-module.exports = require('knex')(config)
+module.exports = require('knex')(config);
