adbd142c7b1ee5fdbd49d01080341a86aa2b150a
Cody Duskin <codyduskin@me.com>
2017-07-26 19:58:01 -0600

    crud

--- index.js
+++ index.js
@@ -2,20 +2,14 @@ const express = require("express")
 const cors = require("cors")
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8080
-const notes = require('./routes')
-
-
+const port = process.env.PORT || 3000
+const routes= require('./routes')
 const app = express()
+
 app.use(cors())
-app.use(bodyParser.json())
-app.use('/notes', notes)
+app.use(bodyParser.urlencoded({encoded: false}))
 
-
-
-
-
+app.use('/notes', routes)
 app.listen(port)
-
 
 module.exports = app;
--- routes.js
+++ routes.js
@@ -3,14 +3,50 @@ const router = express.Router();
 const knex = require('./db/knex')
 
 
-router.get("/", function(req, res) {
-  getNotes().then((data) => {
-    res.json(data)
+router.get("/", (req, res) =>{
+  getNotes().then((response) => {
+    res.json(response)
   })
 });
+router.get('/:id', (req, res)=>{
+  getId(req.params.id).then((response)=>{
+    res.send(response)
+  })
+})
+
+router.post("/", (req, res) =>{
+  let newFam = req.body.fam
+  let newNote = req.body.note
+  let newDate = req.body.date
+  let newPriority = req.body.priority
+  knex('notes').insert({note: newNote, fam: newFam, date: newDate, priority: newPriority}).returning("id")
+  .then((data)=>{
+    res.send(data)
+  })
+})
+
+router.delete("/:id", (req, res)=>{
+  knex('notes').where('id', req.params.id).delete()
+  .then((deleteData)=>{
+    res.send('bye')
+  })
+})
+router.put("/:id", (req, res) =>{
+  let newFam = req.body.fam
+  let newNote = req.body.note
+  let newDate = req.body.date
+  let newPriority = req.body.priority
+  knex('notes').where('id', req.params.id).update({note: newNote, fam: newFam, date: newDate, priority: newPriority}).returning("id")
+  .then((data)=>{
+    res.send(data)
+  })
+})
 
 function getNotes() {
   return knex('*').from('notes')
+}
+function getId (queryId){
+  return knex('notes').where('id', queryId).first()
 }
 
 module.exports = router
